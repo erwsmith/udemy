33e5fedfc972ae6eec628757885bf03aa1227db2
Eric Smith <erwsmith@gmail.com>
2022-10-18 15:20:55 -0600

    nest CommentDetail inside of ApprovalCard

--- react-redux/components/src/index.js
+++ react-redux/components/src/index.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
+import './style/App.css';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                author="Eric" 
-                avatar = {faker.image.image()} 
-                timeAgo="Today at 4:00PM" 
-                content="My comment" 
-            />
-            <CommentDetail 
-                author="Alex" 
-                avatar = {faker.image.image()} 
-                timeAgo="Today at 2:15AM" 
-                content="Great post here" 
-            />
-            <CommentDetail 
-                author="Jane" 
-                avatar = {faker.image.image()} 
-                timeAgo="Yesterday at 5:30AM" 
-                content="Hello, name is Jane" 
-            />
+            <h1>Comment Site</h1>
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                Are you sure you want to do this? 
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Eric" 
+                    avatar = {faker.image.image()} 
+                    timeAgo="Today at 4:00PM" 
+                    content="My comment" 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    avatar = {faker.image.image()} 
+                    timeAgo="Today at 2:15AM" 
+                    content="Great post here" 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    avatar = {faker.image.image()} 
+                    timeAgo="Yesterday at 5:30AM" 
+                    content="Hello, name is Jane" 
+                />
+            </ApprovalCard>
         </div> 
     );
 };
