639cecaf0d2ff01b9725ea852428bd503be14bf1
Eric Smith <erwsmith@gmail.com>
2022-10-18 14:53:48 -0600

    convert and expand props

--- react-redux/components/src/index.js
+++ react-redux/components/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
+            <CommentDetail author="Eric" avatarImage = {faker.image.image()} timeAgo="Today at 4:00PM" comment="My comment" />
+            <CommentDetail author="Alex" avatarImage = {faker.image.image()} timeAgo="Today at 2:15AM" comment="Great post here" />
+            <CommentDetail author="Jane" avatarImage = {faker.image.image()} timeAgo="Yesterday at 5:30AM" comment="Hello, name is Jane" />
         </div> 
     );
 };
