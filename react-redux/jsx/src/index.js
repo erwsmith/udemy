e4717fe094903667f63776956bb61e0c927371aa
Eric Smith <erwsmith@gmail.com>
2022-10-18 13:30:23 -0600

    more notes, examples

--- react-redux/jsx/src/index.js
+++ react-redux/jsx/src/index.js
@@ -12,7 +12,51 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-    return <h1>Hi there!</h1>;
+    // const date = new Date();
+    // const time = date.toLocaleTimeString();
+    // return <h1>{time}</h1>;
+    // Same as 3 lines above:
+    // return <h1>{new Date().toLocaleTimeString()}</h1>;
+    // const name = 'Eric';
+    // const age = '38'
+    // return (
+    //     <div>
+    //         Hi, my name is {name} and my age is {age}
+    //     </div>
+    // )
+    // strings go in ""
+    // numbers go in {}
+    // return <input type="number" min={5} max={10} />;
+    // alternative way to define props
+    // const inputType = "number";
+    // const minValue = 5;
+    // const message = 'Enter age';
+    // return (
+    //     <input 
+    //     style={{ border: '3px solid red' }}
+    //     type={inputType}
+    //     min={minValue}
+    //     max={10}
+    //     list={[1,2,3]}
+    //     alt={message}
+    //     />
+    // );
+
+
+    // 5 rules for HTML to JSX: 
+    // 1. All prop names follow camelCase (e.g. maxLength, autoFocus)
+    // 2. Number attributes use curly braces
+    // 3. Boolean true can be written with just the prop name. False should be
+    // written with curly braces (e.g. <input spellCheck /> ... spellCheck = {false})
+    // 4. The class attribute is 'className'
+    // 5. In-line styles are provided as objects
+
+    // return <textarea autoFocus={true} />;
+    // return <input spellCheck maxLength={8} />;
+    // return <input style={{ border: '3px solid red', paddingTop: '5px', marginBottom: '5px'}} />;
+    return ( 
+        <input placeholder="hi there" />
+    );
 }
 
 // 5) Show the component on the screen
