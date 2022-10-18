1fbf48acb0929ffba93f13b6f7d61be180631a37
Eric Smith <erwsmith@gmail.com>
2022-10-18 14:39:42 -0600

    change to nested component

--- react-redux/components/src/index.js
+++ react-redux/components/src/index.js
@@ -1,54 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
+import { createRoot } from 'react-dom/client';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail />
         </div> 
-    ) 
+    );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+const root = createRoot(document.getElementById('app'));
+root.render(<App />); 
