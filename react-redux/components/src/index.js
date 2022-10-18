7bcd39c1293529f9deee4fc9a4175d0637e7796a
Eric Smith <erwsmith@gmail.com>
2022-10-18 14:59:49 -0600

    change name to 'avatar' from 'avatarImage'

--- react-redux/components/src/index.js
+++ react-redux/components/src/index.js
@@ -6,12 +6,27 @@ import faker from 'faker';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Eric" avatarImage = {faker.image.image()} timeAgo="Today at 4:00PM" comment="My comment" />
-            <CommentDetail author="Alex" avatarImage = {faker.image.image()} timeAgo="Today at 2:15AM" comment="Great post here" />
-            <CommentDetail author="Jane" avatarImage = {faker.image.image()} timeAgo="Yesterday at 5:30AM" comment="Hello, name is Jane" />
+            <CommentDetail 
+                author="Eric" 
+                avatar = {faker.image.image()} 
+                timeAgo="Today at 4:00PM" 
+                content="My comment" 
+            />
+            <CommentDetail 
+                author="Alex" 
+                avatar = {faker.image.image()} 
+                timeAgo="Today at 2:15AM" 
+                content="Great post here" 
+            />
+            <CommentDetail 
+                author="Jane" 
+                avatar = {faker.image.image()} 
+                timeAgo="Yesterday at 5:30AM" 
+                content="Hello, name is Jane" 
+            />
         </div> 
     );
 };
 
 const root = createRoot(document.getElementById('app'));
-root.render(<App />); 
+root.render(<App />);
