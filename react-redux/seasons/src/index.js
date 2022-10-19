4a6d263af1aae8e74e346374dfdbb321005aece5
Eric Smith <erwsmith@gmail.com>
2022-10-19 10:45:54 -0600

    add constructor stub

--- react-redux/seasons/src/index.js
+++ react-redux/seasons/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 // Class based component: 
 class App extends React.Component {
+  // gets called when a new instance of the object is created
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+  }
+
   render () {
     window.navigator.geolocation.getCurrentPosition(
       // Success callback
