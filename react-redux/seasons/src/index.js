398a6df99b22734231f3671b857a2bd870606dca
Eric Smith <erwsmith@gmail.com>
2022-10-19 10:33:02 -0600

    class based component

--- react-redux/seasons/src/index.js
+++ react-redux/seasons/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Functional component: 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    // Success callback
-    (position) => console.log(position), 
-    // Failure callback
-    (err) => console.log(err)
-  );
-
-  return <div>Latitude: {}</div>;
+// Class based component: 
+class App extends React.Component {
+  render () {
+    window.navigator.geolocation.getCurrentPosition(
+      // Success callback
+      (position) => console.log(position), 
+      // Failure callback
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
