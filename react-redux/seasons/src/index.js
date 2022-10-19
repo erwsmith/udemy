5fa087000d74d203334d95a95c2a1f2b9ce68d46
Eric Smith <erwsmith@gmail.com>
2022-10-19 12:27:04 -0600

    add errorMessage and conditional rendering

--- react-redux/seasons/src/index.js
+++ react-redux/seasons/src/index.js
@@ -7,21 +7,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     // this is the only time we do direct assigment to this.state: 
-    this.state = { lat: 'loading...' };
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       // callback function
       position => {
         // the only way to update state is  setState()
-        this.setState({ lat: position.coords.latitude })
+        this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // minimize code inside render function, it gets called a lot
   render () {
-    return <div>Latitude: {this.state.lat} </div>;
+    // conditional rendering
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div> 
+    }
+    return <div>Loading...</div>
   }
 }
 
