5bc6f70d65c55be7ceaa4cf4e22830ba34ebb071
Eric Smith <erwsmith@gmail.com>
2022-10-19 12:03:01 -0600

    add setState for latitude rendering

--- react-redux/seasons/src/index.js
+++ react-redux/seasons/src/index.js
@@ -6,17 +6,21 @@ class App extends React.Component {
   // gets called when a new instance of the object is created
   constructor(props) {
     super(props);
+    // this is the only time we do direct assigment to this.state: 
     this.state = { lat: null };
-  }
 
-  render () {
     window.navigator.geolocation.getCurrentPosition(
-      // Success callback
-      (position) => console.log(position), 
-      // Failure callback
+      (position) => {
+        // the only way to update state is  setState()
+        this.setState({ lat: position.coords.latitude })
+      }, 
       (err) => console.log(err)
     );
-    return <div>Latitude: </div>;
+  }
+
+  // minimize code inside render function, it gets called a lot
+  render () {
+    return <div>Latitude: {this.state.lat} </div>;
   }
 }
 
