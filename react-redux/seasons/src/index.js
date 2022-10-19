3d10316117c4f70543833ad075445993a5348199
Eric Smith <erwsmith@gmail.com>
2022-10-19 12:41:10 -0600

    componentDidMount, componentDidUpdate ...

--- react-redux/seasons/src/index.js
+++ react-redux/seasons/src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom/client';
 
 // Class based component: 
 class App extends React.Component {
-  // gets called when a new instance of the object is created
+  // Gets called when a new instance of the object is created
+  // This is the place for one-time setup
+  // Best practice is to avoid data loading here
   constructor(props) {
     super(props);
-    // this is the only time we do direct assigment to this.state: 
+    // This is the only time we do direct assigment to this.state: 
     this.state = { lat: null, errorMessage: '' };
+  }
 
+  // If component successfully was rendered to screen
+  // Perfect location to load data for component
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      // callback function
+      // Callback function
       position => {
-        // the only way to update state is  setState()
+        // The only way to update state is  setState()
         this.setState({ lat: position.coords.latitude });
       },
       err => {
@@ -21,9 +27,19 @@ class App extends React.Component {
     );
   }
 
-  // minimize code inside render function, it gets called a lot
+  // If component was updated, load data here as required
+  componentDidUpdate() {
+    console.log('My component was updated!')
+  }
+
+  // Cleanup on component
+  componentWillUnmount() {
+
+  }
+
+  // Minimize code inside render function, it gets called a lot, just return JSX
   render () {
-    // conditional rendering
+    // Conditional rendering
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
