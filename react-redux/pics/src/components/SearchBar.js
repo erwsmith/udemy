e7e2db173cd4427d23f5d5ff56e8328ca134a10d
Eric Smith <erwsmith@gmail.com>
2022-10-20 11:47:13 -0600

    onSubmit working

--- react-redux/pics/src/components/SearchBar.js
+++ react-redux/pics/src/components/SearchBar.js
@@ -1,18 +1,44 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    // onInputChange(event) {
+    //     console.log(event.target.value)
+    // }
+
+    // onInputClick() {
+    //     console.log('Input was clicked');
+    // }
+    // and put this prop in the input: onClick={this.onInputClick} 
+
+    // Common user interaction callback functions: 
+    // onClick(), onChange(), onSubmit() ...
+
+    // Creation of a controlled element
+    state = { term: ""};
+
+    // Do this as an arrow function to avoid common 'undefined' error
+    onFormSubmit = event => {
+        // Stop browser from default behavior (reloading)
+        event.preventDefault(); 
+        console.log(this.state.term);
+    }
+
     render () {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input
+                            type="text" 
+                            value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value })}
+                        />
                     </div>
                 </form>
             </div>
         );
     }
-};
+}
 
 export default SearchBar;
