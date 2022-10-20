994c054b1c32b3c9587ea756def3c29f2e8afa1b
Eric Smith <erwsmith@gmail.com>
2022-10-20 14:44:59 -0600

    mostly working input handling

--- react-redux/pics/src/components/App.js
+++ react-redux/pics/src/components/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar'
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: "10px" }}>
-            <SearchBar />
-        </div>
-    );
-};
+
+class App extends React.Component {
+    state = { images: [] };
+    onSearchSubmit = async term => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID _KCjjtPwN3lhrAZGTkO0Boa188y84QMMhN2Ix1c7nKA'
+            }
+        });
+        this.setState({ images: response.data.results });
+    }
+
+    render () {
+        return (
+            <div className="ui container" style={{ marginTop: "10px" }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                Found {this.state.images.length} images.
+            </div>
+        );
+    }
+}
 
 export default App;
