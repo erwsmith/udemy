f2ec1416dec21dda9f22df48f9d31132ba134db3
Eric Smith <erwsmith@gmail.com>
2022-10-21 13:33:02 -0600

    add SearchBar

--- react-redux/videos/src/index.js
+++ react-redux/videos/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
