5570cfce973590e3ca8511281aefe3cdcb2848aa
Eric Smith <erwsmith@gmail.com>
2022-10-22 13:33:51 -0600

    spacing

--- react-redux/videos/src/components/VideoDetail.js
+++ react-redux/videos/src/components/VideoDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const VideoDetail = ({video}) => {
+const VideoDetail = ({ video }) => {
     if (!video) {
         return <div>Loading...</div>
     }
 
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+    
     return (
         <div>
             <div className='ui embed'>
