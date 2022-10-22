9a5efb2be3bb34f38fc5bfe41998f8e58acefb74
Eric Smith <erwsmith@gmail.com>
2022-10-22 14:10:22 -0600

    create some items and connect App and Accordion

--- react-redux/widgets/src/App.js
+++ react-redux/widgets/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end JavaScript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    }, 
+    {
+        title: 'Third Title',
+        conent: 'Third content'
+    }
+]
+
 export default () => {
     return (
         <div>
-            <Accordion />
+            <Accordion items={items}/>
         </div>
     );
 };
