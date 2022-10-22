e47679dc3cf4ada8d21b9c050ff7344cac94b154
Eric Smith <erwsmith@gmail.com>
2022-10-22 14:07:20 -0600

    initial app/component stubs

--- react-redux/widgets/src/App.js
+++ react-redux/widgets/src/App.js
@@ -1,2 +1,10 @@
 import React from 'react';
+import Accordion from './components/Accordion';
 
+export default () => {
+    return (
+        <div>
+            <Accordion />
+        </div>
+    );
+};
