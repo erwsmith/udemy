92c3d268946398abca7449135a9819e3619a25b9
Eric Smith <erwsmith@gmail.com>
2022-10-24 10:21:31 -0600

    add useEffect()

--- react-redux/widgets/src/components/Search.js
+++ react-redux/widgets/src/components/Search.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect }  from 'react';
+import axios from 'axios';
+
 
 const Search  = () => {
     const [term, setTerm] = useState('');
+
+    useEffect(() => {
+        
+    }, [term]);
+    // second argument in useEffect controls when it runs
+    // [] runs only at initial render
+    // ... nothing... runs at initial render and after every rerender
+    // [data] runs at initial render, and after ever rerender if data has changed
 
     return (
         <div>
