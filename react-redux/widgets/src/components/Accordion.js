21b91d380d04fecc3db6484b0985ef5fd338af45
Eric Smith <erwsmith@gmail.com>
2022-10-24 09:35:20 -0600

    accordion component done

--- react-redux/widgets/src/components/Accordion.js
+++ react-redux/widgets/src/components/Accordion.js
@@ -4,7 +4,7 @@ const Accordion = ({ items }) => {
     // useState returns 2 elements
     // first element is the piece of state
     // second element is a function called to update piece of state
-    // useState takes in one argument: the default value for our piece of state
+    // useState takes in one argument: the default value for our piece of state, null in this case
     const [activeIndex, setActiveIndex] = useState(null);
 
     const onTitleClick = (index) => {
@@ -12,16 +12,19 @@ const Accordion = ({ items }) => {
     };
 
     const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : '';
+
         return (
             // instead of a div, return a fragment to avoid double border 
             <React.Fragment key={item.title}>
                 <div 
-                    className='title active'
+                    className={`title ${active}`}
                     onClick={() => onTitleClick(index)}
                     >
-                    <i className='dropdown icon'></i>{item.title}
+                    <i className='dropdown icon'></i>
+                    {item.title}
                 </div>
-                <div className='content active'>
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
@@ -30,7 +33,6 @@ const Accordion = ({ items }) => {
     return (
         <div className='ui styled accordion'>
             {renderedItems}
-            <h1>{activeIndex}</h1>
         </div>
     );
 };
