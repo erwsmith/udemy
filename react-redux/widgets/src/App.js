c126899b4c705ad7264d04fc386a22bbad44736a
Eric Smith <erwsmith@gmail.com>
2022-10-24 10:52:47 -0600

    change to match course code exactly

--- react-redux/widgets/src/App.js
+++ react-redux/widgets/src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front end JavaScript framework'
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is a favorite JS library among engineers'
-    }, 
-    {
-        title: 'Why is React useful for engineers?',
-        content: 'React makes it simple to build websites and apps, create UI test cases, reuse existing codes on the website to its mobile counterpart, and improve the UI and performance of web applications. '
-    }
-]
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
-export default () => {
-    return (
-        <div>
-            <br />
-            <Search />
-        </div>
-    );
+const App = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
 };
+export default App;
