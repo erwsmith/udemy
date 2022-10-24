37d9f713fa80f6bd9191d499dd3206de82edfb8e
Eric Smith <erwsmith@gmail.com>
2022-10-24 09:43:32 -0600

    update items[]

--- react-redux/widgets/src/App.js
+++ react-redux/widgets/src/App.js
@@ -11,8 +11,8 @@ const items = [
         content: 'React is a favorite JS library among engineers'
     }, 
     {
-        title: 'Third Title',
-        content: 'Third content'
+        title: 'Why is React useful for engineers?',
+        content: 'React makes it simple to build websites and apps, create UI test cases, reuse existing codes on the website to its mobile counterpart, and improve the UI and performance of web applications. '
     }
 ]
 
