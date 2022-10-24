0c1e7137cc5f61afba70ed8590f9b92eedc86ff5
Eric Smith <erwsmith@gmail.com>
2022-10-23 21:51:10 -0600

    add useState Hook

--- react-redux/widgets/src/components/Accordion.js
+++ react-redux/widgets/src/components/Accordion.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
+    // useState returns 2 elements
+    // first element is the piece of state
+    // second element is a function called to update piece of state
+    // useState takes in one argument: the default value for our piece of state
+    const [activeIndex, setActiveIndex] = useState(null);
+
     const onTitleClick = (index) => {
-        console.log('Title clicked', index);
+        setActiveIndex(index)
     };
 
     const renderedItems = items.map((item, index) => {
@@ -24,6 +30,7 @@ const Accordion = ({ items }) => {
     return (
         <div className='ui styled accordion'>
             {renderedItems}
+            <h1>{activeIndex}</h1>
         </div>
     );
 };
