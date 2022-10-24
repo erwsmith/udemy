e8f7f07bad926cddeb24503f96b96d30efa98e6d
Eric Smith <erwsmith@gmail.com>
2022-10-24 10:57:16 -0600

    change return() to match course code

--- react-redux/widgets/src/components/Search.js
+++ react-redux/widgets/src/components/Search.js
@@ -3,10 +3,27 @@ import axios from 'axios';
 
 
 const Search  = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
+
+    console.log(results);
 
     useEffect(() => {
-        
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search', 
+                    origin: '*', 
+                    format: 'json',
+                    srsearch: term
+                },
+            });
+            setResults(data.query.search);
+        };
+        if (term) {
+            search();
+        }
     }, [term]);
     // second argument in useEffect controls when it runs
     // [] runs only at initial render
@@ -15,18 +32,18 @@ const Search  = () => {
 
     return (
         <div>
-            <div className='ui form'>
-                <div className='field'>
-                    <label>Enter Search Term</label>
-                    <input
-                        value={term}
-                        onChange={e => setTerm(e.target.value)}
-                        className='input' 
-                    /> 
-                </div>
+          <div className="ui form">
+            <div className="field">
+              <label>Enter Search Term</label>
+              <input
+                value={term}
+                onChange={(e) => setTerm(e.target.value)}
+                className="input"
+              />
             </div>
+          </div>
         </div>
-    );
+      );
 }
 
 export default Search;
