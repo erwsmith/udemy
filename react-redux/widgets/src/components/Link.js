08ffdd6c3ebcbb6bd345cf779a6ebf4cc98bf9df
Eric Smith <erwsmith@gmail.com>
2022-10-24 16:59:24 -0600

    popStateEvent, load in new tab

--- react-redux/widgets/src/components/Link.js
+++ react-redux/widgets/src/components/Link.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
     const onClick = (event) => {
+        // if user is holding command or ctrl key 
+        if (event.metaKey || event.ctrlKey ) {
+            // reloads the page clicked in a new tab
+            return; 
+        }
+
         event.preventDefault();
         window.history.pushState({}, '', href)
+
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
     };
 
     return <a onClick={onClick} className={className} href={href}>{children}</a>
