83360d98dbcda2f2356ff63781438a4274e9a8e4
Eric Smith <erwsmith@gmail.com>
2022-10-24 14:34:40 -0600

    add Translate, move dropDown ->child of Translate

--- react-redux/widgets/src/App.js
+++ react-redux/widgets/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
-import Dropdown from "./components/Dropdown";
+// import Accordion from "./components/Accordion";
+// import Search from "./components/Search";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -34,20 +34,11 @@ const options = [
 ];
 
 const App = () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
   return (
     <div>
-        <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-      {showDropdown ? 
-        <Dropdown 
-        selected={selected} 
-        onSelectedChange={setSelected}
-        options={options} 
-      /> : null
-        }
+        <Translate />
     </div>
   );
 };
+
 export default App;
