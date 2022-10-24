c84da2cef86c9e107a927044f3e589ddeb334ba9
Eric Smith <erwsmith@gmail.com>
2022-10-24 17:00:28 -0600

    setCurrentPath and add/remove listener

--- react-redux/widgets/src/components/Route.js
+++ react-redux/widgets/src/components/Route.js
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
+
 const Route = ({ path, children }) => {
-    return window.location.pathname === path 
+    // this state is just to get our route to update
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const onLocationChange = () => {
+            setCurrentPath(window.location.pathname);
+        };
+
+        window.addEventListener('popstate', onLocationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange);
+        };
+    }, []);
+
+    return currentPath === path 
     ? children 
     : null;
 };
