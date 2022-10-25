87762bfe4e0c206e804d5a6b91a3df313f7ca4a1
Eric Smith <erwsmith@gmail.com>
2022-10-25 13:42:23 -0600

    finish basic/standard connect

--- react-redux/songs/src/components/songList.js
+++ react-redux/songs/src/components/songList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends React.Component {
     render () {
+        // this.props === { songs: state.songs }
         return <div>SongList</div>
     }
 }
 
-export default SongList;
+// Conventional name for this function is mapStateToProps
+// Get all data from redux store (state)
+const mapStateToProps = state => {
+    return { songs: state.songs };
+}
+
+// Call function that is nested in another function with ()()
+export default connect(mapStateToProps)(SongList);
