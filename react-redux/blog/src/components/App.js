af90e816667f47e2b67ba5fc7b9fd0b756f46321
Eric Smith <erwsmith@gmail.com>
2022-10-26 13:57:58 -0600

    initial setup done

---
+++ react-redux/blog/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+    return (
+      <div className="ui container">
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+                App col0
+            </div>
+            <div className="five wide column">
+                App col1
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default App;
