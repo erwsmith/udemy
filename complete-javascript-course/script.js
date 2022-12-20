2cff525980e9d1890d60d9272541bc6bf4735bfa
Eric Smith <erwsmith@gmail.com>
2022-12-20 12:13:22 -0700

    .

--- complete-javascript-course/script.js
+++ complete-javascript-course/script.js
@@ -25,25 +25,25 @@
 // Data Bonus 2: 
 // Dolphins score 97, 112 and 101. 
 // Koalas score 109, 95 and 106
-class Team {
-    constructor(scores){
-        this.scores = scores;
-    }
+// class Team {
+//     constructor(scores){
+//         this.scores = scores;
+//     }
 
-    // Getter 
-    get avg() {
-        return this.calcAvg();
-    }
+//     // Getter 
+//     get avg() {
+//         return this.calcAvg();
+//     }
 
-    // Method
-    calcAvg() {
-        return this.scores.reduce((a, b) => a + b) / this.scores.length;
-    }
-}
+//     // Method
+//     calcAvg() {
+//         return this.scores.reduce((a, b) => a + b) / this.scores.length;
+//     }
+// }
 
 // Nobody, scores too low
-dolphins = new Team([96, 108, 89])
-koalas = new Team([88, 91, 110])
+// dolphins = new Team([96, 108, 89])
+// koalas = new Team([88, 91, 110])
 
 // Koalas win
 // dolphins = new Team([97,112,101])
@@ -53,14 +53,30 @@ koalas = new Team([88, 91, 110])
 // dolphins = new Team([97,112,101])
 // koalas = new Team([109,95,106])
 
-console.log(dolphins.avg, koalas.avg)
+// console.log(dolphins.avg, koalas.avg)
 
-if (dolphins.avg > koalas.avg && dolphins.avg > 100) {
-    console.log('Dolphins win!')
-} else if (dolphins.avg < koalas.avg && koalas.avg > 100) {
-    console.log('Koalas win!')
-} else if (dolphins.avg === koalas.avg) {
-    console.log("It's a draw!")
-} else {
-    console.log('Scores too low!')
-}
+// if (dolphins.avg > koalas.avg && dolphins.avg > 100) {
+//     console.log('Dolphins win!')
+// } else if (dolphins.avg < koalas.avg && koalas.avg > 100) {
+//     console.log('Koalas win!')
+// } else if (dolphins.avg === koalas.avg) {
+//     console.log("It's a draw!")
+// } else {
+//     console.log('Scores too low!')
+// }
+
+
+// let text = "   Hello world!";
+// let result = text.trim();
+
+// console.log(text, "end here")
+// console.log(result)
+
+//We define the text variable that needs to be cleansed of line breaks.
+let someText = "Here's some text.\nIt has some line breaks that will be\n removed \r using Javascript.\r\n";
+someText = "Line one.\nLine two.\nLine three.\n "
+console.log(someText)
+
+//This javascript code removes all 3 types of line breaks
+someText = someText.replace(/(\r\n|\n|\r)/gm,"");
+console.log(someText)
