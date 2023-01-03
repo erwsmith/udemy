53b23b56ed6a9b3be56e2d53b883f00f04d750c9
Eric Smith <erwsmith@gmail.com>
2023-01-03 13:33:17 -0700

    testing done

--- go_complete/cards/deck_test.go
+++ go_complete/cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 // function takes in t *testing.T arg by default
 func TestNewDeck(t *testing.T) {
@@ -16,4 +19,20 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d) - 1] != "Four of Clubs" {
 		t.Errorf("Expected Four of Clubs, but got %v", d[len(d) - 1])
 	}
+}
+
+
+func TestSaveToDeckAndNewDeckTestFromFile(t * testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
