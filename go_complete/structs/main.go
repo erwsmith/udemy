8b64fe60f089c046fe08faa6fe476f8038947ef7
Eric Smith <erwsmith@gmail.com>
2023-01-04 12:03:54 -0700

    pointer basics

--- go_complete/structs/main.go
+++ go_complete/structs/main.go
@@ -14,8 +14,14 @@ type person struct {
 	lastName string
 	adminStatus bool
 	employeeID int
+	// type contactInfo is defined elsewhere and can be added as a person field like this: 
 	contactInfo
 }
+
+
+// turn value into address with '&'
+// turn address into value with '*'
+
 
 func main() {
 	// create a value of type 'person'
@@ -29,22 +35,24 @@ func main() {
 			zipCode: 84105,
 		},
 	}
+	// Enable updating of alex by using a pointer
+	// '&' gives the memory address of the variable that follows
+	// alexPointer := &alex
+	// alexPointer.updateName("Alexandra")
 
-	// Alternative way to create variable 'alex' of type person
-	// var alex person
-	// alex.firstName = "Alex"
-	// alex.lastName = "Anderson"
-	// alex.adminStatus = true
-	// alex.employeeID = 1
-	// alex.contact = contactInfo{email: "alex@example.com", zipCode: 84105}
-
+	// Shortcut to do the above, since we use *person in the receiver in updateName()
 	alex.updateName("Alexandra")
+
 	alex.print()
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+// '*' when put before a type is different from '*' in front of a variable.
+// '*person' in the receiver below is the type 'pointer to person'
+func (pointerToPerson *person) updateName(newFirstName string) {
+	// '*' gives us the value at the memory address
+	(*pointerToPerson).firstName = newFirstName
 }
+
 
 func (p person) print() {
 	fmt.Printf("%+v", p)
