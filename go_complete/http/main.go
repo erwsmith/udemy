922fbe5b2ebc4e6c2de559f66e11d653b79588c4
Eric Smith <erwsmith@gmail.com>
2023-01-06 13:08:04 -0700

    abbreviate/correct way to copy data

--- go_complete/http/main.go
+++ go_complete/http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -11,12 +12,15 @@ func main() {
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
-	} 
-	
-	// create a byte slice with 99999 empty elements
-	bs := make([]byte, 99999)
-	// read the body of the response into the byte slice 'bs'
-	resp.Body.Read(bs)
-	// print bs as a string
-	fmt.Println(string(bs))
+	}
+
+	// // create a byte slice with 99999 empty elements
+	// bs := make([]byte, 99999)
+	// // read the body of the response into the byte slice 'bs'
+	// resp.Body.Read(bs)
+	// // print bs as a string
+	// fmt.Println(string(bs))
+
+	// same as above 3 lines, but better
+	io.Copy(os.Stdout, resp.Body)
 }
