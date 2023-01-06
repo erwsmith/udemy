26befbbf5b49f506ed9e835f61b7c4b40e0a7b78
Eric Smith <erwsmith@gmail.com>
2023-01-06 12:53:46 -0700

    Reader basics

--- go_complete/http/main.go
+++ go_complete/http/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -11,7 +11,12 @@ func main() {
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
-	} else {
-		fmt.Println(resp)
-	}
+	} 
+	
+	// create a byte slice with 99999 empty elements
+	bs := make([]byte, 99999)
+	// read the body of the response into the byte slice 'bs'
+	resp.Body.Read(bs)
+	// print bs as a string
+	fmt.Println(string(bs))
 }
