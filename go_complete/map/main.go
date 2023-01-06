11cf7b8836bbde4c33a90d551cea94082b1642d5
Eric Smith <erwsmith@gmail.com>
2023-01-06 10:03:26 -0700

    iterate through map

--- go_complete/map/main.go
+++ go_complete/map/main.go
@@ -7,17 +7,26 @@ func main() {
 	// var colors map[string]string
 	// colors := make(map[string]string)
 
-	// create a map with string keys and string values.
+	// create a map with string keys and string values, and populate with 
+	// key value pairs
 	colors := map[string]string{
 		"red": "#ff0000", 
-		"blue": "4bf745",
+		"blue": "#4bf745",
+		"white": "#ffffff",
 	}
 
 	// Add a key value pair to map
-	colors["white"] = "ffffff"
+	// colors["white"] = "ffffff"
 
 	// remove pair from map
-	delete(colors, "white")
+	// delete(colors, "white")
 
-	fmt.Println(colors)
+	// fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Printf("Hex code for %v is %v.\n", color, hex)
+	}
 }
